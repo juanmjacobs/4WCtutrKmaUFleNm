61f8d8b0b75e0e274adf102910fc2e8cef9c4108
Juan Jacobs <juan.marcos.jacobs@grada.com.ar>
2016-07-06 16:42:16 -0300

    Cambiando mensaje

--- jobs/sellerListingsUpdater/src/run.js
+++ jobs/sellerListingsUpdater/src/run.js
@@ -2,4 +2,4 @@ var service = require('./sellerListingsUpdaterService');
 service.start();
 console.log('Recuerda configurar la URL de la API de listing tracker. Por default es http://localhost:9000')
 console.log('Recuerda que la API de listing tracker debe estar levantada para que el proceso funcione')
-console.log('Monitorea el progreso con "tail -f serviceLog.txt"')
+console.log('Monitorea el progreso con el archivo "serviceLog.txt" en este directorio')
