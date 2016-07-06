1a0687f33b11c176a79ff04fee55d11ee6a61ba2
Juan Jacobs <juan.marcos.jacobs@grada.com.ar>
2016-07-06 02:39:02 -0300

    Agregando API url a constants

--- jobs/sellerListingsUpdater/config/constants.js
+++ jobs/sellerListingsUpdater/config/constants.js
@@ -3,5 +3,6 @@ module.exports = Object.freeze({
     SIMULTANEOUS_REQUESTS: 5,
     SELLER_ID: 154901871,
 	LOCAL_LISTING_TRACKER_URL:"http://localhost:9000",
+	ML_API_URL: 'https://api.mercadolibre.com',
 	ML_SEARCH_URL: 'https://api.mercadolibre.com/sites/MLM/search'
 });
