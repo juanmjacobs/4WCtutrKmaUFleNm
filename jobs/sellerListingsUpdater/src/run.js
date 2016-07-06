0cfee77fab46ad746ee7f9e7c5e4e7d1bdac7da5
Juan Jacobs <juan.marcos.jacobs@grada.com.ar>
2016-07-06 16:31:39 -0300

    Mensajes del proceso run

--- jobs/sellerListingsUpdater/src/run.js
+++ jobs/sellerListingsUpdater/src/run.js
@@ -1,2 +1,5 @@
 var service = require('./sellerListingsUpdaterService');
 service.start();
+console.log('Recuerda configurar la URL de la API de listing tracker. Por default es http://localhost:9000')
+console.log('Recuerda que la API de listing tracker debe estar levantada para que el proceso funcione')
+console.log('Monitorea el progreso con "tail -f serviceLog.txt"')
